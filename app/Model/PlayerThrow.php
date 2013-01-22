3dba6f275a045b8f2c2408e66d212792093e657f
Kasper Baagø Jensen <kapper14@gmail.com>
2013-01-22 09:36:49 +0100

    Some logic added for player rules

--- app/Model/PlayerThrow.php
+++ app/Model/PlayerThrow.php
@@ -14,16 +14,65 @@ class PlayerThrow extends AppModel
         'foreignKey' => 'game_id'
     ));
 
-    private $round = 0;
-    private $score = 0;
-
-    public function addScore($score, $playerid, $round) {
-        $this->score = $score;
-        $this->round = $round;
-        $this->Player->load($playerid);
+    /**
+     * Adds a new score to the game
+     * @param array $saveArray
+     */
+    public function addScore(Array $saveArray) {
+        $this->data['score'] = $saveArray['score'];
+        $this->data['player_id'] = $saveArray['player_id'];
+        $this->data['game_id'] = $saveArray['game_id'];
+        $this->data['round'] = $saveArray['round'];
+        $this->data['arrow'] = $saveArray['arrow'];
+        $this->save($this->data);
     }
 
-    public function save($data = NULL, $validate = true, $fieldList = Array()) {
+    /**
+     * Get lates round added to round
+     * @param int $gameID
+     * @return int
+     */
+    public function getRound($gameID, $playerID) {
+        $res = $this->find('first', array(
+            'condition' => array('PlayerThrow.game_id' => $gameID, 'PlayerThrow.player_id' => $playerID),
+            'order' => array('PlayerThrow.modified DESC')
+        ));
 
+        if($res == false) {
+            $round = 0;
+        } else {
+            $round = $res['PlayerThrow']['round'];
+        }
+
+        if($this->getArrow($gameID, $playerID) == 0) {
+            return $round + 1;
+        } else {
+            return $round;
+        }
+    }
+
+    /**
+     * Returns arrow there are thrown
+     * @param $gameID
+     * @param $playerID
+     * @return int
+     */
+    public function getArrow($gameID, $playerID) {
+        $res = $this->find('first', array(
+            'condition' => array('PlayerThrow.game_id' => $gameID, 'PlayerThrow.player_id' => $playerID),
+            'order' => array('PlayerThrow.modified DESC')
+        ));
+
+        if($res == false) {
+            $arrow = 0;
+        } else {
+            $arrow = $res['PlayerThrow']['arrow'];
+        }
+
+        if($arrow >= 3) {
+            return 0;
+        }
+
+        return $arrow + 1;
     }
 }
